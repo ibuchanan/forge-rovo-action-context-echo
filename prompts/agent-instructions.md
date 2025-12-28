d17d217e5ad981bb112f1eea00b72fca8ffeebad
Ian Buchanan <ibuchanan@atlassian.com>
2025-12-28 14:46:26 -0500

    feat: echo request

---
+++ prompts/agent-instructions.md
@@ -0,0 +1,6 @@
+You are an echo utility.
+You respond with JSON to show
+what was sent to the Forge App
+including the Rovo context.
+When you respond with JSON,
+put it into a codeblock for better presentation.
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-export { trigger } from "./forge/trigger";
 export { lifecycle } from "./forge/lifecycle";
+
+import type { EchoContextPayload } from "./actionpayload";
+import { truncateEvents } from "./forge/logging";
+import {
+  buildResponse,
+  type Response,
+  type WebtriggerEvent,
+} from "./forge/trigger";
+
+export async function trigger(req: WebtriggerEvent): Promise<Response> {
+  console.debug(
+    `Context token (invocation identification) for invocation of trigger: ${truncateEvents(req.contextToken)}`,
+  );
+  /*
+  const projectRequest = JSON.parse(req.body) as FetchProjectWbsPayload;
+  projectRequest.context = req.context;
+  const yamlDoc = await fetchWbsContentFromProject(projectRequest);
+  */
+  const res: Response = buildResponse();
+  // console.debug(`response: ${JSON.stringify(res)}`);
+  return res;
+}
+
+export function echoRequest(req: EchoContextPayload): string {
+  console.debug(`req: ${truncateEvents(req)}`);
+  return JSON.stringify(req);
+}
